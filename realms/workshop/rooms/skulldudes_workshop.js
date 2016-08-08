a7e3ea32c096b70c54328e54f8e233c73a19bf4f
khkwan0@gmail.com <khkwan0@gmail.com>
2016-08-08 20:45:58 +0000

    more file structure

---
+++ realms/workshop/rooms/skulldudes_workshop.js
@@ -0,0 +1,10 @@
+room = {
+    "name":"skulldudes_workshop",
+    "realm":"workshop",
+    "long":"This is Skulldudes's workshop.  You notice there is a large gun safe in the room that is cracked open.  Peeking in you see quite an arsenal of weaponry.  It's a cozy little workshop.  You also notice a big black dildo in the corner of the room",
+    "short": "Skulldudes's Cozy Little Workshop",
+    "exits": {
+    },
+    "persist": 1,
+    "inv": {}
+}
