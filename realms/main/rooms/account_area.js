1e8f8640bc66d4fae4bfeff00c3e7763893babca
khkwan0@gmail.com <khkwan0@gmail.com>
2016-08-08 09:53:30 +0000

    moved rooms to realms tree

---
+++ realms/main/rooms/account_area.js
@@ -0,0 +1,7 @@
+room = {
+    "name":"account_area",
+    "realm":"main",
+    "short":"Accounting Area",
+    "long":"You are in the accounting area.",
+    "exits":{"n":"outside_ted"}
+}
---
+++ realms/main/rooms/outside_ted.js
@@ -0,0 +1,7 @@
+room = {
+    "name":"outside_ted",
+    "realm":"main",
+    "long":"You are standing outside of Ted's office on the fifth floor of the 9000 building.  It is a beautiful, clear day in Southern California.  The area is brightly lit by the white tiles and floor to ceiling windows.  Just south of you is the accounting area while just a few steps west will bring you in front of the executive shiiter.",
+    "short": "Outside of Ted's Office",
+    "exits": {'w':'hallway_exec_bathroom', 's':'account_area','enter':'teds_office'}
+}
