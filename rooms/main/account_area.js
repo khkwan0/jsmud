13ca465cfb434feb1bc11afe628172df69e84a49
khkwan0@gmail.com <khkwan0@gmail.com>
2016-08-08 09:55:57 +0000

    removed rooms, replaced with realms

--- rooms/main/account_area.js
+++
@@ -1,7 +0,0 @@
-room = {
-    "name":"account_area",
-    "realm":"main",
-    "short":"Accounting Area",
-    "long":"You are in the accounting area.",
-    "exits":{"n":"outside_ted"}
-}
--- rooms/main/hallway_exec_bathroom.js
+++
@@ -1,9 +0,0 @@
-room = {
-    "name":"hallway_exec_bathroom",
-    "realm":"main",
-    "long":"A strange odor emanates from the executive bathroom.  The door to the bathroom is closed and you can smell a foul odor coming from the other side.  The odor is a mixture of fresh poop and Febreeze.  Your eyes start to water.  Ted's office is a couple of steps East.",
-    "short": "In front of the executive bathroom",
-    "exits": {
-        'e':'outside_ted'
-    }
-}
