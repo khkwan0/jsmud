c68fd1a779f352e4f04cc93dd38cf38a22ec3bed
khkwan0@gmail.com <khkwan0@gmail.com>
2016-08-11 18:52:08 +0000

    added more attributes to the sample object

--- realms/main/objs/knife.js
+++ realms/main/objs/knife.js
@@ -1,6 +1,9 @@
 {
     "name":"knife",
     "alias":"knife",
+    "long":"This is a decorated shiny knife with 6-inch blade.  You can use this for many different applications.  It's hilt is decorated with strange tribal symbols.",
+    "short":"A 6-inch knife with a decorated hilt.",
+    "public": true
     "invisibile":false,
     "can_get":true
 }
